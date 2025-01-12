23e68be88d7d035ba87094a00b8616b50d09306c
Simfort <lemondzavadavid265@gmail.com>
2025-01-12 20:30:09 +0300

    added:fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -10,7 +11,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
 ];
 
 export default eslintConfig;
